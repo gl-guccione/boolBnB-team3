7c1ec17054bcc3207d72e87dec8d71582948097d
gl-guccione <g.guccione3@gmail.com>
2020-11-20 17:24:03 +0100

    Done Migration UserTable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstname', '50');
+            $table->string('lastname', '50');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->date('date_of_birth');
+            $table->string('avatar');
+            $table->text('description');
             $table->timestamps();
         });
     }
