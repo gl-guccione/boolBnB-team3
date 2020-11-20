3c9bcf86881183cf3ff70524f1f69266278d8bc4
gl-guccione <g.guccione3@gmail.com>
2020-11-20 18:07:50 +0100

    Done Migration Payments + rm timestamp

---
+++ database/migrations/2020_11_20_170153_create_payments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->string('transaction_id', 8)->unique();
+            $table->float('amount', 4, 2);
+            $table->string('status', 30);
+            $table->dateTime('date_of_payment');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
