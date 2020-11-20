9779417ced296ab6fa1ce054d2a0dbe911f28222
peppe965 <giuseppe.falco000@gmail.com>
2020-11-20 18:47:26 +0100

    added routes & controller

---
+++ app/Http/Controllers/Admin/SponsorshipController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SponsorshipController extends Controller
+{
+      /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,38 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-// Route::prefix('admin')->name('admin.')->namespace('Admin')->middleware('auth')->group(function () {
-//   Route::get('/', 'HomeController@index')->name('home');
-// });
+//routes for UPR/UPRA
+Route::prefix('admin')->name('admin.')->namespace('Admin')->middleware('auth')->group(function () {
+    //home = index flats
+    // Route::get('/', 'HomeController@index')->name('home');
+
+    //flats routes
+    Route::resource('flats', 'FlatController');
+    
+    //requests routes
+    // Route::resource('requests', 'RequestController');
+    Route::get('requests', 'RequestController@index')->name('requests.index');
+    Route::get('requests/{id}', 'RequestController@show')->name('requests.show');
+    Route::get('requests', 'RequestController@store')->name('requests.store');
+    Route::get('requests/{request}', 'RequestController@destroy')->name('requests.destroy');
+    Route::get('requests/create', 'RequestController@create')->name('requests.create');
+    
+    //sponsorships routes
+    Route::get('sponsorships', 'RequestController@index')->name('sponsorships.index');
+    Route::get('sponsorships/{id}', 'RequestController@show')->name('sponsorships.show');
+    Route::get('sponsorships', 'RequestController@store')->name('sponsorships.store');
+    Route::get('sponsorships/create', 'RequestController@create')->name('sponsorships.create');
+    
+
+    //statistic route
+    Route::get('flats/stat', 'ViewController@index')->name('view');
+});
+
+
+//routes for UI
+Route::name('guest.')->namespace('Guest')->group(function () {
+    Route::get('/flats', 'FlatController@index')->name('flat.index');
+    Route::get('/flats/{slug}', 'FlatController@show')->name('flat.show');
+
+
+});
