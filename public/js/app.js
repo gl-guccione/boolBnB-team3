45e4d806799b74bf71bf33e2db796e7330316f6e
Alessandro <alessandro.boscato@gmail.com>
2020-11-20 17:23:40 +0100

    created sass/partials & views/partials

--- public/js/app.js
+++ public/js/app.js
@@ -37339,8 +37339,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/lucaguccione/Dropbox/boolean.careers/boolBnB-team3/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/lucaguccione/Dropbox/boolean.careers/boolBnB-team3/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/alessandroboscato/Desktop/boolean/Esercitazioni/boolBnB-team3/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/alessandroboscato/Desktop/boolean/Esercitazioni/boolBnB-team3/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
