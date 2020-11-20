59738736333ec91a573445874bb36cc050933aed
gl-guccione <g.guccione3@gmail.com>
2020-11-20 17:44:29 +0100

    Done Migration FlatsTable

--- database/migrations/2020_11_20_162633_create_flats_table.php
+++ database/migrations/2020_11_20_162633_create_flats_table.php
@@ -15,6 +15,24 @@ class CreateFlatsTable extends Migration
     {
         Schema::create('flats', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('title')->unique();
+            $table->string('slug')->unique();
+            $table->boolean('active')->default(true);
+            $table->tinyInteger('number_of_rooms');
+            $table->tinyInteger('number_of_beds');
+            $table->tinyInteger('number_of_bathrooms');
+            $table->smallInteger('mq');
+            $table->float('price', 6, 2);
+            $table->string('type', 30);
+            $table->text('description');
+            $table->tinyInteger('stars');
+            $table->string('extra_options');
+            $table->string('street_name');
+            $table->string('zip_code', 5);
+            $table->string('city');
+            $table->string('lat', 15);
+            $table->string('lng', 15);
             $table->timestamps();
         });
     }
