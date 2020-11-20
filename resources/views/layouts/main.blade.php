d791af19512198ea87ed7445a0706ee4f8ce5b33
dom <domepiro87@yahoo.it>
2020-11-20 19:24:04 +0100

    added sass/partial - view/navbar
    ho aggiunto file nel sass/partials e definito le view/layouts/partial. In piu` ho creato una nuova view homeprova collegando il layouts e una navbar per verificare se tutto funzionava

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,18 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="{{asset('css/app.css')}}">
-  <title>@yield('title')</title>
-</head>
-<body>
-    @include('partials.header')
-  <main>
-    @yield('mainContent')
-  </main>
-    @include('partials.footer')
+@include('layouts.partials.head')
 
+<body>
+    <nav>
+        @include('layouts.partials.navbar')
+    </nav>
+    
+    <header>
+        @include('layouts.partials.jumbo')
+    </header>
+
+      
+    <main>
+        @yield('mainContent')
+    </main>
+
+    <footer>
+        @include('layouts.partials.footer')
+    </footer>
+
+    <!-- script -->
+    <script src="{{asset('js/app.js')}}"></script>
 </body>
+
 </html>
--- resources/views/partials/footer.blade.php
+++
@@ -1,3 +0,0 @@
-<footer>
-  ciao ciao
-</footer>
