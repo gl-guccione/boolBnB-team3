8af2ff16c4007dbf5b223a2ea26c15fd8098b654
dom <domepiro87@yahoo.it>
2020-11-21 17:49:26 +0100

    create page sass/layouts

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,22 +1,13 @@
 @include('layouts.partials.head')
 
 <body>
-    <nav>
-        @include('layouts.partials.navbar')
-    </nav>
-    
-    <header>
-        @include('layouts.partials.jumbo')
-    </header>
 
-      
-    <main>
+        @include('layouts.partials.header')
+
         @yield('mainContent')
-    </main>
+        @yield('content')
 
-    <footer>
         @include('layouts.partials.footer')
-    </footer>
 
     <!-- script -->
     <script src="{{asset('js/app.js')}}"></script>
---
+++ resources/views/ui/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('mainContent')
+<main>
+    <div class="img-carousel">
+        {{-- Jumbotron --}}
+    </div>
+
+    <div class="presentazione"></div>
+    
+    <div class="inEvidenza"></div>
+</main>
+@endsection
