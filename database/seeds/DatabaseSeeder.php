86ee8d18941c63a34f7275a3a46c032d6e9f7ebc
gl-guccione <g.guccione3@gmail.com>
2020-11-22 19:31:03 +0100

    Done Images Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,7 +17,8 @@ class DatabaseSeeder extends Seeder
           UsersTableSeeder::class,
           FlatsTableSeeder::class,
           RequestsTableSeeder::class,
-          ViewsTableSeeder::class
+          ViewsTableSeeder::class,
+          ImagesTableSeeder::class
         ]);
     }
 }
