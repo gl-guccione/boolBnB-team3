a12b447c58c9e36133269c2998b3401e5a565883
gl-guccione <g.guccione3@gmail.com>
2020-11-22 18:47:00 +0100

    Done Views Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
           UsersTableSeeder::class,
           FlatsTableSeeder::class,
-          RequestsTableSeeder::class
+          RequestsTableSeeder::class,
+          ViewsTableSeeder::class
         ]);
     }
 }
