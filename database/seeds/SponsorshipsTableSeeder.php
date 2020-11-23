dfc7837d61e6423a15a2b1c18198fc555fc936b7
gl-guccione <g.guccione3@gmail.com>
2020-11-23 21:48:48 +0100

    Fix - if payment->status == 'confirmed'

--- database/seeds/SponsorshipsTableSeeder.php
+++ database/seeds/SponsorshipsTableSeeder.php
@@ -25,46 +25,50 @@ class SponsorshipsTableSeeder extends Seeder
 
         foreach ($payments as $payment) {
 
-          $new_sponsorship = New Sponsorship;
+          if ($payment->status == 'confirmed') {
 
-          $random_flat = Flat::inRandomOrder()->first();
-          $new_sponsorship->flat_id = $random_flat->id;
+            $new_sponsorship = New Sponsorship;
 
-          $new_sponsorship->payment_id = $payment->id;
+            $random_flat = Flat::inRandomOrder()->first();
+            $new_sponsorship->flat_id = $random_flat->id;
 
-          $amount = $payment->amount;
-          $sponsorship_price = SponsorshipPrice::where('price', $amount)->first();
-          $new_sponsorship->sponsorship_price_id = $sponsorship_price->id;
+            $new_sponsorship->payment_id = $payment->id;
+
+            $amount = $payment->amount;
+            $sponsorship_price = SponsorshipPrice::where('price', $amount)->first();
+            $new_sponsorship->sponsorship_price_id = $sponsorship_price->id;
 
 
-          // check if exist an other sponsorship for the same flat (in desc order, selecting only the first)
-          $sponsorship_same_flat = Sponsorship::orderByDesc('date_of_end')->where('flat_id', $new_sponsorship->flat_id)->first();
+            // check if exist an other sponsorship for the same flat (in desc order, selecting only the first)
+            $sponsorship_same_flat = Sponsorship::orderByDesc('date_of_end')->where('flat_id', $new_sponsorship->flat_id)->first();
 
-          // if exist i set the date_of_start of the new sponsorship as the date_of_end of the last sponsorship (only if it is in the future), otherwise i set it as the date_of_payment
-          if ($sponsorship_same_flat == null) {
-
-            $new_sponsorship->date_of_start = $payment->date_of_payment;
-
-          } else {
-
-            $date_of_payment = Carbon::parse($payment->date_of_payment);
-            $old_sponsorship_date = Carbon::parse($sponsorship_same_flat->date_of_end);
-
-            if ($date_of_payment->gt($old_sponsorship_date)) {
+            // if exist i set the date_of_start of the new sponsorship as the date_of_end of the last sponsorship (only if it is in the future), otherwise i set it as the date_of_payment
+            if ($sponsorship_same_flat == null) {
 
               $new_sponsorship->date_of_start = $payment->date_of_payment;
 
             } else {
 
-              $new_sponsorship->date_of_start = $sponsorship_same_flat->date_of_end;
+              $date_of_payment = Carbon::parse($payment->date_of_payment);
+              $old_sponsorship_date = Carbon::parse($sponsorship_same_flat->date_of_end);
+
+              if ($date_of_payment->gt($old_sponsorship_date)) {
+
+                $new_sponsorship->date_of_start = $payment->date_of_payment;
+
+              } else {
+
+                $new_sponsorship->date_of_start = $sponsorship_same_flat->date_of_end;
+
+              }
 
             }
 
+            $new_sponsorship->date_of_end = Carbon::parse($new_sponsorship->date_of_start)->addHours($sponsorship_price->duration_in_hours);
+
+            $new_sponsorship->save();
+
           }
-
-          $new_sponsorship->date_of_end = Carbon::parse($new_sponsorship->date_of_start)->addHours($sponsorship_price->duration_in_hours);
-
-          $new_sponsorship->save();
 
         }
     }
