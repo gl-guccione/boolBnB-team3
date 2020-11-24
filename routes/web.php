a0afeb5b2aae0b8005ed86241e88170e2e00e6a6
peppe965 <giuseppe.falco000@gmail.com>
2020-11-24 16:34:07 +0100

    edit routes

--- routes/web.php
+++ routes/web.php
@@ -24,26 +24,23 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 //routes for UPR/UPRA
 Route::prefix('admin')->name('admin.')->namespace('Admin')->middleware('auth')->group(function () {
-    //home = index flats
-    // Route::get('/', 'HomeController@index')->name('home');
 
     //flats routes
     Route::resource('flats', 'FlatController');
     
-    //requests routes
-    Route::get('requests', 'RequestController@index')->name('requests.index');
-    Route::get('requests/{id}', 'RequestController@show')->name('requests.show');
-    Route::get('requests/{request}', 'RequestController@destroy')->name('requests.destroy');
+    //messages routes
+    Route::get('messages', 'RequestController@index')->name('messages.index');
+    Route::get('messages/{id}', 'RequestController@show')->name('messages.show');
     
     //sponsorships routes
     Route::get('sponsorships', 'RequestController@index')->name('sponsorships.index');
     Route::get('sponsorships/{id}', 'RequestController@show')->name('sponsorships.show');
-    Route::get('sponsorships', 'RequestController@store')->name('sponsorships.store');
     Route::get('sponsorships/create', 'RequestController@create')->name('sponsorships.create');
+    Route::get('sponsorships', 'RequestController@store')->name('sponsorships.store');
     
 
-    //statistic route
-    Route::get('flats/stat', 'ViewController@index')->name('view');
+    //statistics route
+    Route::get('flats/statistics', 'ViewController@index')->name('statistics');
 });
 
 
@@ -54,9 +51,9 @@ Route::name('guest.')->namespace('Guest')->group(function () {
     Route::get('/', 'FlatController@index')->name('flat.index');
     Route::get('/flats/{slug}', 'FlatController@show')->name('flat.show');
 
-    //routes for request public
-    Route::get('requests/create', 'RequestController@create')->name('requests.create');
-    Route::get('requests', 'RequestController@store')->name('requests.store');
-    Route::get('requests/{id}', 'RequestController@show')->name('requests.show');
+    //routes for message public
+    Route::get('messages/create', 'RequestController@create')->name('messages.create');
+    Route::get('messages', 'RequestController@store')->name('messages.store');
+    Route::get('messages/{id}', 'RequestController@show')->name('messages.show');
 
 });
