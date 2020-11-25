946f5c2fb06aa1aaa172f9ead876a8b8fe5a5014
marco <marcodecrema@libero.it>
2020-11-25 16:52:48 +0100

    done index flats view

--- resources/views/admin/flats/index.blade.php
+++ resources/views/admin/flats/index.blade.php
@@ -1,1 +1,27 @@
-@dd($flats)
+@extends ('layouts.app')
+
+@section('content')
+
+    <h1 class="">Area privata - I miei appartamenti</h1>
+
+
+   
+@foreach ($flats as $flat) 
+    <h2>{{$flat->title}}</h2>
+    <img src={{$flat->images()->first()->path}} />
+    <p>{{$flat->description}}</p>
+    <span> Valutazione: {{$flat->stars}}</span>
+    <a href="#">Sponsorizza</a>
+    <a href="#">Edit</a>
+    <form action="" method="POST">  
+        @csrf
+        @method('DELETE')
+        <button type="submit">Delete</button>
+    </form>
+    <a href="">Contatti</a>
+@endforeach
+
+
+
+@endsection
+
