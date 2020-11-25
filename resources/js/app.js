d5ddf3621044568ba214eddab4e6aa55e2147d18
Alessandro <alessandro.boscato@gmail.com>
2020-11-25 18:36:10 +0100

    half done create flats view

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
 require('./bootstrap');
+var places = require('places.js');
+
+(function() { 
+  var placesAutocomplete = places({ 
+    container: document.querySelector('#address') 
+  });  
+  var $address = document.querySelector('#address-value') 
+    placesAutocomplete.on('change', function(e) { 
+      $address.textContent = e.suggestion.value 
+      console.log(e.suggestion.latlng); 
+    });  
+    placesAutocomplete.on('clear', function() { 
+    $address.textContent = 'none'; 
+  });  
+})();
