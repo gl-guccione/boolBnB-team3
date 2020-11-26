5c49ffd705b31a1ec6f0598d41e3952bed0739e3
gl-guccione <g.guccione3@gmail.com>
2020-11-26 14:41:04 +0100

    Query for flats with sponsorship

--- app/Http/Controllers/Guest/HomepageController.php
+++ app/Http/Controllers/Guest/HomepageController.php
@@ -1,10 +1,16 @@
 <?php
 
+// defining Namespace
 namespace App\Http\Controllers\Guest;
 
+// using Laravel Facades
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+// using Carbon
+use Carbon\Carbon;
+
+// using Models
 use App\Flat;
 use App\Option;
 
@@ -18,6 +24,11 @@ class HomepageController extends Controller
     public function index()
     {
         $flats = Flat::inRandomOrder()->limit(10)->get();
+
+        $datetime_now = Carbon::now();
+        $sponsorhip_flats = Flat::whereHas('sponsorships', function ($query) use ($datetime_now) {
+         $query->where('date_of_end', '>', $datetime_now);
+        })->get();
 
         return view('guest.home', compact('flats'));
     }
