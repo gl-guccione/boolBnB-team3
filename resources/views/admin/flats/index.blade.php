9cfaaea3e5f8cb814008103fd063d593876c8745
dom <domepiro87@yahoo.it>
2020-11-26 18:06:12 +0100

    fix

--- resources/views/admin/flats/index.blade.php
+++ resources/views/admin/flats/index.blade.php
@@ -2,49 +2,44 @@
 
 @section('content')
 <div class="container">
-  <div class="row">
-    <h1 class="col-lg-12">Area privata - I miei appartamenti</h1>
+  <h1 class="col-lg-12">Area privata - I miei appartamenti</h1>
 
-    @foreach ($flats as $flat)
-
-    <div class="col-lg-2 pt-4 m-3">
-      <div class="img-flats">
-        <h3>image</h3>
-        <img src="{{ $flat->images[0]->path }}">
+  @foreach ($flats as $flat)
+    <div class="row">
+      <div class="col-lg-2 pt-4 m-3">
+        <div class="img-flats">
+          <h3>image</h3>
+          <img style="max-width: 100px" src="{{ $flat->images[0]->path }}">
+        </div>
       </div>
-    </div>
 
-    <div class="card col-lg-6 pt-4 m-3">
-      <h2>{{ $flat->title }}</h2>
+      <div class="card col-lg-6 pt-4 m-3">
+        <h2>{{ $flat->title }}</h2>
 
-        <a href="{{ route('admin.flats.show', $flat->slug) }}">Dettagli</a>
+          <a href="{{ route('admin.flats.show', $flat->slug) }}">Dettagli</a>
 
-        <p>{{ $flat->description }}</p>
+          <p>{{ $flat->description }}</p>
 
-        <span> Valutazione: {{ $flat->stars }}</span>
+          <span> Valutazione: {{ $flat->stars }}</span>
       </div>
       <div class="card__icons col-lg-3 pt-4">
-        <div class="sponsorship m-2">
-          <a href="#"><i class="fas fa-shopping-cart"></i></a>
-        </div>
-        <div class="edit m-2">
-          <a href="{{ route('admin.flats.edit', $flat->slug) }}"><i class="fas fa-edit"></i></a>
-        </div>
-        <div class="delite m-2">
-          <form action="{{ route('admin.flats.destroy', $flat->slug) }}" method="POST">
+          <div class="sponsorship m-2">
+            <a href="#"><i class="fas fa-shopping-cart"></i></a>
+          </div>
+          <div class="edit m-2">
+            <a href="{{ route('admin.flats.edit', $flat->slug) }}"><i class="fas fa-edit"></i></a>
+          </div>
 
-              @csrf
-              @method('DELETE')
+          <div class="delete m-2">
+              <a href={{ route('admin.flats.destroy', $flat->slug) }}"><i class="fas fa-trash-alt"></i></a>
+          </div>
 
-              <button type="submit">Delete</button>
-
-          </form>
-        </div>
-        <div class="message m-2">
-          <a href="#"><i class="fas fa-envelope"></i></a>
-        </div>
+          <div class="message m-2">
+            <a href="#"><i class="fas fa-envelope"></i></a>
+          </div>
       </div>
-    @endforeach
-  </div>
+      
+    </div>
+  @endforeach
 </div>
 @endsection
