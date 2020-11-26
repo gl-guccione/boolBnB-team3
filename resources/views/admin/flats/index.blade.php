8ad4217784544ab45dd69096935a324ea4c1a6d2
dom <domepiro87@yahoo.it>
2020-11-26 18:49:57 +0100

    fix effect

--- resources/views/admin/flats/index.blade.php
+++ resources/views/admin/flats/index.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
 <div class="container">
-  <h1 class="col-lg-12">Area privata - I miei appartamenti</h1>
+  <h1 class="col-lg-12 title">I miei appartamenti</h1>
 
   @foreach ($flats as $flat)
-    <div class="row">
+    <div class="row flats">
       <div class="col-lg-2 pt-4 m-3">
         <div class="img-flats">
           <h3>image</h3>
@@ -14,9 +14,9 @@
       </div>
 
       <div class="card col-lg-6 pt-4 m-3">
-        <h2>{{ $flat->title }}</h2>
+        <h2 class="card__title"><a href="{{ route('admin.flats.show', $flat->slug) }}">{{ $flat->title }}</a></h2>
 
-          <a href="{{ route('admin.flats.show', $flat->slug) }}">Dettagli</a>
+          {{-- <a href="{{ route('admin.flats.show', $flat->slug) }}">Dettagli</a> --}}
 
           <p>{{ $flat->description }}</p>
 
