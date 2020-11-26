14f5bde0eb125a63b2220f81bd8f942f72ff47e8
peppe965 <giuseppe.falco000@gmail.com>
2020-11-26 16:30:24 +0100

    fix app.js error

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,19 @@
 require('./bootstrap');
+
 var places = require('places.js');
 
-(function() { 
-  var placesAutocomplete = places({ 
-    container: document.querySelector('#address') 
-  });  
-  var $address = document.querySelector('#address-value') 
-    placesAutocomplete.on('change', function(e) { 
-      $address.textContent = e.suggestion.value 
-      console.log(e.suggestion.latlng); 
-    });  
-    placesAutocomplete.on('clear', function() { 
-    $address.textContent = 'none'; 
-  });  
+(function(){
+  var placesAutocomplete = places({
+    container: document.querySelector('#address')
+  })
+
+  var $address = document.querySelector('#address-value')
+  placesAutocomplete.on('change', function(e){
+    $address.textContent = e.suggestion.value
+    console.log(e.suggestion.latlng);
+  })
+
+  placesAutocomplete.on('clear', function(){
+    $address.textContent = 'none';
+  })
 })();
