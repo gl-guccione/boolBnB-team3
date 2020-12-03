8afe7912618e2598686d93860e56de1e59a5b30b
marco <marcodecrema@libero.it>
2020-12-03 18:28:11 +0100

    desktop media query users show view

--- resources/views/guest/users/show.blade.php
+++ resources/views/guest/users/show.blade.php
@@ -10,38 +10,38 @@
 
     {{-- user info --}}
     <div class="row">
-    <div class="user_container">
-        <div class="col-xl-3 col-lg-4 col-sm-12 col-md-4 avatar ">
+    
+        <div class="col-xl-3 col-lg-4 col-sm-5 col-md-4 col-xs-12 avatar">
           <img src="{{$user->avatar}}" alt="avatar utente">
         </div>
 
-        <div class="col-xl-9 col-lg-8 col-sm-12 col-md-8 user_infos">
+        <div class="col-xl-9 col-lg-8 col-sm-7 col-md-8 col-xs-12 user_infos">
           <h3 class="inline_bl">{{$user->firstname}} {{$user->lastname}}</h3>
           <h4>{{$user->description}}</h4>
         </div>
-      </div>
+      
     </div>
     {{-- /user info --}}
 
     @if (count($user->flats) > 0)
       {{-- flats list--}}
-    <div class="row">
+    <div class="row title">
       <div class="col-12">
         <h2>I miei appartamenti:</h2>
       </div>
     </div>
 
-    <div class="row">
+    <div class="">
       <div class="flats_list">
 
         @foreach($user->flats as $flat)
-          <div class="flat-box col-lg-12 col-sm-12 col-md-12">
+          <div class="row flat-box">
           {{-- flat --}}
-            <div class="col-lg-4 col-md-12 col-xl-12 col-xs-12 flat-img ">
+            <div class="col-lg-4 col-md-4 col-xl-4 col-sm-12 col-xs-12 flat-img ">
               <img src="{{ asset('storage/'.$flat->images[0]->path) }}" alt="foto appartamento">
             </div>
 
-            <div class="col-lg-8 col-md-12 col-xl-12 col-xs-12 flat-info">
+            <div class="col-lg-8 col-md-8 col-xl-8 col-sm-12 col-xs-12 flat-info">
               <h3>{{$flat->title}}</h3>
               <p>{{$flat->description}}</p>
 
