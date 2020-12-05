3206d8f9ec7130334d726f59c030c093e8e344c2
gl-guccione <g.guccione3@gmail.com>
2020-12-05 22:10:47 +0100

    fix

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -46,11 +46,7 @@
     @endif
 
   </div>
-
   {{-- /wrapper --}}
-  {{-- <script src="https://unpkg.com/aos@next/dist/aos.js"></script> --}}
-  {{-- <script>
-    AOS.init();
-  </script> --}}
+
 
 @endsection
