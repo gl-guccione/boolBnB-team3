a4653afa9c7a155c7c062351dc42b1560298a170
gl-guccione <g.guccione3@gmail.com>
2020-12-06 01:49:40 +0100

    wireframe - index flats

--- resources/views/admin/flats/index.blade.php
+++ resources/views/admin/flats/index.blade.php
@@ -5,130 +5,182 @@
 @section('content')
 
 {{-- container --}}
-<div class="container flats-admin">
+<div class="container py-4">
 
-  <h2 class="col-lg-12">Ciao {{ $flats[0]->user->firstname }} {{ $flats[0]->user->lastname }}</h2>
-  <h1 class="col-lg-12 title">Area Privata - I miei appartamenti</h1>
+  <div class="hero py-6">
+    <img class="hero__image pt-4" src="{{ asset('img/flats/flat.svg') }}" alt="flat image">
+    <h1 class="hero__title py-4">
+      Buongiorno,
+      <span class="hero__name">
+        {{ $flats[0]->user->firstname }} {{ $flats[0]->user->lastname }}!
+        <span class="underline"></span>
+      </span>
+      <br>
+      Benvenuto nella tua area privata.
+    </h1>
+  </div>
 
   {{-- flats --}}
   @foreach ($flats as $flat)
 
     {{-- row --}}
-    <div class="row flats">
+    <div class="row">
 
-      {{-- column image --}}
-      <div class="img-flats-hover col-lg-2 m-3">
+      {{-- first col --}}
+      <div class="col-lg-8 flat__first">
 
-        @if (count($flat->images) > 0)
+        <div class="flat__textimage d_flex">
 
-            <img class="img-flats" src="{{ asset('storage/'.$flat->images[0]->path) }}">
+          @if (count($flat->images) > 0)
+            <img class="flat__image @if($flat->active == 0) inactive @endif" src="{{ asset('storage/'.$flat->images[0]->path) }}" alt="flat image">
+          @endif
 
-        @endif
+          <div class="flat__text">
+
+            <h2 class="flat__title">
+              <a class="flat__link overflow_row" href="{{ route('guest.flats.show', $flat->slug) }}">{{ $flat->title }}</a>
+            </h2>
+
+            <p class="flat__description overflow_two_rows">
+              {{ $flat->description }}
+            </p>
+
+            <p>
+              {{ $flat->street_name }},
+              {{ $flat->zip_code }} -
+              {{ $flat->city }}
+            </p>
+
+            <span class="flat__stars">
+
+              @php
+                if ($flat->stars % 2 == 0) {
+                  $star = $flat->stars / 2;
+                  $half_star = 0;
+                } else {
+                  $star = intval($flat->stars / 2);
+                  $half_star = 1;
+                }
+              @endphp
+
+              @for ($i = 0; $i < $star; $i++)
+                <i class="star_color fas fa-star"></i>
+              @endfor
+              @for ($i = 0; $i < $half_star; $i++)
+                <i class="star_color fas fa-star-half"></i>
+              @endfor
+
+              ({{ $flat->stars / 2 }})
+
+            </span>
+
+          </div>
+
+        </div>
+
+        <div class="flat__info d_flex">
+
+          <div>
+            <i class="fas fa-door-open"></i>
+            {{ $flat->number_of_rooms }}
+          </div>
+
+          <div>
+            <i class="fas fa-bed"></i>
+            {{ $flat->number_of_beds }}
+          </div>
+
+          <div>
+            <i class="fas fa-restroom"></i>
+            {{ $flat->number_of_bathrooms }}
+          </div>
+
+          <div>
+            <i class="fas fa-border-style"></i>
+            {{ $flat->mq }}m²
+          </div>
+
+          <div>
+            <i class="fas fa-euro-sign"></i>
+            {{ $flat->price }}€
+          </div>
+
+        </div>
 
       </div>
-      {{-- /column image --}}
+      {{-- /first col --}}
 
-      {{-- column info --}}
-      <div class="card col-lg-6 pt-4 m-3">
+      {{-- second col --}}
+      <div class="col-lg-2 flat__second">
 
-        <h2 class="card__title">
-          <a href="{{ route('guest.flats.show', $flat->slug) }}">{{ $flat->title }}</a>
-        </h2>
-
-        <p class="p__description">{{ $flat->description }}</p>
-
-        <span>
-
-          @php
-            if ($flat->stars % 2 == 0) {
-              $star = $flat->stars / 2;
-              $half_star = 0;
-            } else {
-              $star = intval($flat->stars / 2);
-              $half_star = 1;
-            }
-          @endphp
-
-          @for ($i = 0; $i < $star; $i++)
-            <i class="star_color fas fa-star"></i>
-          @endfor
-          @for ($i = 0; $i < $half_star; $i++)
-            <i class="star_color fas fa-star-half"></i>
-          @endfor
-
-          ({{ $flat->stars / 2 }})
-
-        </span>
-
-      </div>
-      {{-- /column info --}}
-
-      {{-- column actions --}}
-      <div class="card__icons col-lg-3 pt-4">
-
-        {{-- sponsor button --}}
-        <div class="sponsorship m-2">
-          {{-- TODO add route for sponsorship --}}
-          <a href="#">
-            <i class="fas fa-shopping-cart"></i>
+        <div class="flat__sponsored">
+          <span>In evidenza fino al 21/12/2020</span>
+        </div>
+        <hr>
+        <div class="flat__sponsor">
+          <a href="{{ route('admin.sponsorships.create') }}">
+            <i class="fas fa-credit-card"> Promuovi</i>
           </a>
         </div>
-        {{-- /sponsor button --}}
-
-        {{-- edit button --}}
-        <div class="edit m-2">
-          <a href="{{ route('admin.flats.edit', $flat->slug) }}">
-            <i class="fas fa-edit"></i>
-          </a>
-        </div>
-        {{-- /edit button --}}
-
-        {{-- pause button/form --}}
-        <form class="m-2" action="{{ route('admin.flats.update_status', $flat->slug) }}" method="POST">
-
-          @csrf
-          @method('PUT')
-
-          <button type="submit">
-            @if ($flat->active == 1)
-              <i class="fas fa-pause"></i>
-            @else
-              <i class="fas fa-play"></i>
-            @endif
-          </button>
-
-        </form>
-        {{-- /pause button/form --}}
-
-        {{-- delete button/form --}}
-        <form class="delete m-2" action="{{ route('admin.flats.destroy', $flat->slug) }}" method="POST">
-
-          @csrf
-          @method('DELETE')
-
-          <button class="delete" type="submit">
-            <i class="fas fa-trash-alt"></i>
-          </button>
-
-        </form>
-        {{-- /delete button/form --}}
-
-        {{-- message button --}}
-        {{-- <div class="message m-2"> --}}
-
-          {{-- TODO add route to see all the message for the flat --}}
-          {{-- <a href="#">
-            <i class="fas fa-envelope"></i>
-          </a> --}}
-
-        {{-- </div> --}}
-        {{-- /message button --}}
 
       </div>
-      {{-- /column actions --}}
+      {{-- /second col --}}
+
+      {{-- third col --}}
+      <div class="col-lg-2 flat__third">
+
+        <div class="flat_actions">
+
+          {{-- pause button/form --}}
+          <form class="m-2" action="{{ route('admin.flats.update_status', $flat->slug) }}" method="POST">
+
+            @csrf
+            @method('PUT')
+
+            <button type="submit">
+              @if ($flat->active == 1)
+                <i class="fas fa-pause"></i>
+                Disattiva
+              @else
+                <i class="fas fa-play"></i>
+                Attiva
+              @endif
+            </button>
+
+          </form>
+          {{-- /pause button/form --}}
+
+          {{-- edit button --}}
+          <div class="edit m-2">
+            <a href="{{ route('admin.flats.edit', $flat->slug) }}">
+              <i class="fas fa-edit"></i>
+              Modifica
+            </a>
+          </div>
+          {{-- /edit button --}}
+
+          {{-- delete button/form --}}
+          <form class="delete m-2" action="{{ route('admin.flats.destroy', $flat->slug) }}" method="POST">
+
+            @csrf
+            @method('DELETE')
+
+            <button class="delete" type="submit">
+              <i class="fas fa-trash-alt"></i>
+              Elimina
+            </button>
+
+          </form>
+          {{-- /delete button/form --}}
+
+        </div>
+
+      </div>
+      {{-- /third col --}}
 
     </div>
     {{-- /row --}}
+
 
   @endforeach
   {{-- /flats --}}
