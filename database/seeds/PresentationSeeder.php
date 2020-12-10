b585bbfbf60752a4adac0d41ed35ad42339a4b40
gl-guccione <g.guccione3@gmail.com>
2020-12-10 19:34:18 +0100

    Added users

--- database/seeds/PresentationSeeder.php
+++ database/seeds/PresentationSeeder.php
@@ -38,12 +38,73 @@ class PresentationSeeder extends Seeder
                       [
                         'firstname' => 'Luca',
                         'lastname' => 'Guccione',
-                        'email' => 'g.guccione3@gmail.com',
-                        'password' => 'password',
                         'date_of_birth' => '1994-04-19',
-                        'avatar' => 'link avatar',
-                        'description' => 'descrizione'
-                      ]
+                        'avatar' => '/storage/media/avatar/avatar_u_1.png',
+                        'description' => 'Amo la mia terra e la mia cultura ed il desiderio di condividerla è al centro stesso di questo progetto. Sarò per cui felicemente a disposizione per aiutare ad accedere ad attività escursionistiche, sportive, enogastronomiche.'
+                      ],
+                      [
+                        'firstname' => 'Giuseppe',
+                        'lastname' => 'Falco',
+                        'date_of_birth' => '1993-07-23',
+                        'avatar' => '/storage/media/avatar/avatar_u_2.png',
+                        'description' => 'Sono felice di poter essere d’aiuto per i miei ospiti tramite messaggio per qualsiasi richiesta o info, la modalità per l’accoglienza è tramite self check in!'
+                      ],
+                      [
+                        'firstname' => 'Alessandro',
+                        'lastname' => 'Boscato',
+                        'date_of_birth' => '1991-10-29',
+                        'avatar' => '/storage/media/avatar/avatar_u_3.png',
+                        'description' => 'Sono Alessandro e sono anche io un host su Roma. Insieme a mia moglie ci piace viaggiare e conoscere nuovi luoghi in cambio avrete gentilezza, sorrisi e cura dei vostri alloggi.'
+                      ],
+                      [
+                        'firstname' => 'Marco',
+                        'lastname' => 'De Crema',
+                        'date_of_birth' => '1992-09-01',
+                        'avatar' => '/storage/media/avatar/avatar_u_4.png',
+                        'description' => 'Se solo fossimo pienamente padroni del nostro tempo... Brevemente intendo che la nostra è una vita frenetica! Tra tutto il resto, sempre di più, si afferma questa passione ad accogliere "altre storie", intrecciare altre vite. Preferiamo proprio quelle come le nostre, veloci e tuttavia attente, comunicative ma senza troppe chiacchiere. Siate i benvenuti quindi, capiremo di voi dalla prima occhiata! E vi offriremo quello che possiamo: speriamo sia abbastanza!'
+                      ],
+                      [
+                        'firstname' => 'Domenico',
+                        'lastname' => 'Garofalo',
+                        'date_of_birth' => '1998-01-13',
+                        'avatar' => '/storage/media/avatar/avatar_u_5.png',
+                        'description' => 'Saremo lieti di essere presenti e a disposizione durante la vostra vacanza, qualora ne avrete bisogno. Una buona vacanza ha sempre inizio con un ottimo check-in. Saremo in grado di fornirvi tutti gli strumenti necessari per non farvi stressare e per non farvi perdere tempo, alla ricerca di informazioni che noi sapremo darvi in anticipo. Vi daremo i nostri contatti telefonici, anche wuotsapp e Viber, per essere sempre a vostra disposizione per qualsiasi necessità o semplice informazione. Non aspettare, prenota ora!'
+                      ],
+                      [
+                        'firstname' => 'Carla',
+                        'lastname' => 'Artale',
+                        'date_of_birth' => '1990-03-11',
+                        'avatar' => '/storage/media/avatar/avatar_d_1.png',
+                        'description' => 'Siamo una famiglia con casa a Marina di Cecina, costruita da mio padre nel 1968. La locazione turistica di due porzioni è affidata a Claudia, mia figlia. Roberto, il suo ex marito, è di nazionalità Svizzera e all\'occorrenza aiuta con i turisti dall\'estero. Amiamo la natura e gli animali, abbiamo gatti e cani, giardino e un piccolo angolo aromatiche e orto. Ci piace leggere, scattare foto, cucinare, ascoltare musica. Roberto parla fluentemente svizzero-tedesco, tedesco, inglese e francese.'
+                      ],
+                      [
+                        'firstname' => 'Lucilla',
+                        'lastname' => 'Costantini',
+                        'date_of_birth' => '1988-01-08',
+                        'avatar' => '/storage/media/avatar/avatar_d_2.png',
+                        'description' => 'Sono una giovane mamma che ama viaggiare. Durante uno dei nostri viaggi in Alaska (più di 10 anni fa!) siamo stati ospiti per la prima volta in un B&B gestito da una gentile signora.....Da quella esperienza abbiamo deciso di aprire la nostra casa ai viaggiatori di tutto il mondo :) Amo la natura selvaggia, e il mio stile di viaggio è con lo zaino in spalla. Vorrei poter visitare ogni angolo della Terra, e in viaggio riesco a sentirmi pienamente libera: essenzialmente sono una backpaker! Mangiare, leggere un libro davanti al camino con un bicchiere di un buon vino, vedere un bel film fanno parte delle gioie quotidiane! I miei ospiti si sentiranno a casa propria (almeno è quello il mio intento!), e con loro vorrei instaurare un rapporto di fiducia e convivialità.'
+                      ],
+                      [
+                        'firstname' => 'Federica',
+                        'lastname' => 'Palumbo',
+                        'date_of_birth' => '1999-06-15',
+                        'avatar' => '/storage/media/avatar/avatar_d_3.png',
+                        'description' => 'Sarei felicissima di essere la vostra guida durante il vostro soggiorno nella bellissima Napoli. Sono a disposizione per qualsiasi tipo di consiglio su cose da fare e posti da visitare e soprattutto ristoranti dove è possibile assaggiare i veri sapori di Napoli!'
+                      ],
+                      [
+                        'firstname' => 'Graziella',
+                        'lastname' => 'Fontana',
+                        'date_of_birth' => '1987-12-13',
+                        'avatar' => '/storage/media/avatar/avatar_d_4.png',
+                        'description' => 'Sono una persona solare - Adoro dipingere - Amo la mia famiglia e i miei figli Roberta e Marco - Ascolto tutta la musica ma in particolare i cantautori italiani e le canzoni "senza tempo" - Leggo romanzi e mi piace vedere i film tratti dagli stessi libri - Mi piace la gente, mi piace conoscerla e mi piace imparare da loro cose nuove.'
+                      ],
+                      [
+                        'firstname' => 'Carla',
+                        'lastname' => 'Artale',
+                        'date_of_birth' => '1990-08-27',
+                        'avatar' => '/storage/media/avatar/avatar_d_5.png',
+                        'description' => 'Sono nata in Calabria, anche se ho vissuto molti anni al nord Italia e per periodi anche all’estero. Ho una bellissima famiglia con un maschietto ed una femminuccia adorabili. Viviamo alla contrada Scinà, in un piano diverso da quello destinato ai nostri ospiti, i quali hanno accesso alla loro struttura da un’entrata indipendente. E’ una zona tranquilla, per la pace, il relax e le vacanze in ogni stagione, anche se d’estate è molto più popolata da turisti di ogni parte d’Italia e moltissimi anche dall’estero. Il mare vicino ci offre uno spettacolare incontro con lo stretto di Messina, dove è possibile ammirare ad occhio nudo la Sicilia ed il vulcano Stromboli. Mi piacciono gli animali, la natura e mangiare sano, per questo abbiamo il nostro piccolo giardino. E’un luogo dove i bambini godono di aria pulita e giocano in serenità.'
+                      ],
                     ];
 
           foreach ($users as $user) {
@@ -51,8 +112,8 @@ class PresentationSeeder extends Seeder
 
             $new_user->firstname = $user['firstname'];
             $new_user->lastname = $user['lastname'];
-            $new_user->email = $user['email'];
-            $new_user->password = Hash::make($user['password']);
+            $new_user->email = $user['firstname'].$user['lastname'].'@gmail.com';
+            $new_user->password = Hash::make('password');
             $new_user->date_of_birth = $user['date_of_birth'];
             $new_user->avatar = $user['avatar'];
             $new_user->description = $user['description'];
