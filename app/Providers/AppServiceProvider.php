62613fb424a95bb9a93ac4bc3e4470299dce74a9
gl-guccione <g.guccione3@gmail.com>
2020-12-13 21:10:10 +0100

    fix https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-      \URL::forceScheme('https');
+      if (env('APP_ENV') === 'production') {
+        URL::forceSchema('https');
+      }
     }
 
     /**
